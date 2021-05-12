dbaa640842d55b44cb5be40a89df9ed139036174
swatisingh16 <84076296+swatisingh16@users.noreply.github.com>
2021-05-12 16:42:09 +0530

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+int a , b;
+    a=5 , b=10;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("swapping numbers of a and b are: %d, %d", a , b);
+    
+return 0;
+}
